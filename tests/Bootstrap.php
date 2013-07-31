cebad0197ca37dfbbcea98e595cf3083fba9437d
Marco Pivetta <ocramius@gmail.com>
2013-07-31 21:08:47 +0200

    Introducing athletic specific performance test

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -35,5 +35,6 @@ if (! isset($loader)) {
 /* @var $loader \Composer\Autoload\ClassLoader */
 $loader->add('GeneratedHydratorTest\\', __DIR__);
 $loader->add('GeneratedHydratorTestAsset\\', __DIR__);
+$loader->add('GeneratedHydratorPerformance\\', __DIR__);
 
 unset($files, $file, $loader);
